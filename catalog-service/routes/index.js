2e26994d0c69fe7679ca158b08a015b1ee30a909
rhodinemma <rhodinemma10@gmail.com>
2023-11-13 22:03:35 +0300

    feat: add the catalog service business logic and test endpoints

--- catalog-service/routes/index.js
+++ catalog-service/routes/index.js
@@ -1,11 +1,74 @@
 const express = require("express");
 
+const CatalogService = require("../lib/CatalogService");
+
 const router = express.Router();
 
-// Define your RESTful routes here
-router.get("/", (req, res) => {
-  // Return a JSON response with a 'hello world' message
-  res.json({ msg: "hello world" });
+router.get("/items", async (req, res) => {
+  try {
+    const items = await CatalogService.getAll();
+    return res.json(items);
+  } catch (error) {
+    console.error("Error getting items:", error);
+    return res.status(500).json({ error: "Failed to get items" });
+  }
+});
+
+router.get("/items/:itemId", async (req, res) => {
+  try {
+    const item = await CatalogService.getOne(req.params.itemId);
+    if (!item) {
+      return res.status(404).json({ error: "Item not found" });
+    }
+
+    return res.json(item);
+  } catch (error) {
+    console.error("Error getting item:", error);
+    return res.status(500).json({ error: "Failed to get item" });
+  }
+});
+
+router.post("/items", async (req, res) => {
+  try {
+    const newItem = await CatalogService.create(req.body);
+    return res.json(newItem);
+  } catch (error) {
+    console.error("Error creating item:", error);
+    return res.status(500).json({ error: "Failed to create item" });
+  }
+});
+
+router.put("/items/:itemId", async (req, res) => {
+  try {
+    const updatedItem = await CatalogService.update(
+      req.params.itemId,
+      req.body
+    );
+
+    if (!updatedItem) {
+      return res.status(404).json({ error: "Item not found" });
+    }
+
+    return res.json(updatedItem);
+  } catch (error) {
+    console.error("Error creating item:", error);
+    return res.status(500).json({ error: "Failed to create item" });
+  }
+});
+
+router.delete("/items/:itemId", async (req, res) => {
+  try {
+    const deletionResult = await CatalogService.remove(req.params.itemId);
+
+    if (deletionResult.deletedCount === 0) {
+      return res.status(404).json({ error: "Item not found" });
+    }
+
+    return res.sendStatus(204);
+  } catch (error) {
+    console.error("Error deleting item:", error);
+    return res.status(500).json({ error: "Failed to delete item" });
+  }
 });
 
 module.exports = router;
