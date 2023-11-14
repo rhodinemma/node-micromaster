2c598aa782773fde7418131de95c82c38100efbc
rhodinemma <rhodinemma10@gmail.com>
2023-11-14 22:58:19 +0300

    feat: register/update a service with registry

--- registry-service/routes/index.js
+++ registry-service/routes/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 
+const Registry = require("../lib/Registry");
+
+const registry = new Registry();
+
 const router = express.Router();
 
+// Register and Update a service
 router.put(
   "/register/:servicename/:serviceversion/:serviceport",
   (req, res, next) => {
-    return next("Not implemented");
+    const { servicename, serviceversion, serviceport } = req.params;
+
+    let serviceip = req.ip;
+    if (serviceip.includes("::1") || serviceip.includes("::ffff:127.0.0.1")) {
+      serviceip = "127.0.0.1";
+    }
+
+    const key = registry.register(
+      servicename,
+      serviceversion,
+      serviceip,
+      serviceport
+    );
+
+    return res.json({ result: key });
   }
 );
 
