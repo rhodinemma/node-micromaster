6d45f1dab1118159772e008fd11a04c59466d183
rhodinemma <rhodinemma10@gmail.com>
2023-11-14 23:50:50 +0300

    feat: functionality to unregister a service from the registry

--- registry-service/routes/index.js
+++ registry-service/routes/index.js
@@ -6,16 +6,26 @@ const registry = new Registry();
 
 const router = express.Router();
 
+function getRequestArguments(req) {
+  let serviceip = req.ip;
+  if (serviceip.includes("::1") || serviceip.includes("::ffff:127.0.0.1")) {
+    serviceip = "127.0.0.1";
+  }
+
+  return {
+    servicename: req.params.servicename,
+    serviceversion: req.params.serviceversion,
+    serviceport: req.params.serviceport,
+    serviceip
+  };
+}
+
 // Register and Update a service
 router.put(
   "/register/:servicename/:serviceversion/:serviceport",
   (req, res, next) => {
-    const { servicename, serviceversion, serviceport } = req.params;
-
-    let serviceip = req.ip;
-    if (serviceip.includes("::1") || serviceip.includes("::ffff:127.0.0.1")) {
-      serviceip = "127.0.0.1";
-    }
+    const { servicename, serviceversion, serviceport, serviceip } =
+      getRequestArguments(req);
 
     const key = registry.register(
       servicename,
@@ -31,7 +41,17 @@ router.put(
 router.delete(
   "/register/:servicename/:serviceversion/:serviceport",
   (req, res, next) => {
-    return next("Not implemented");
+    const { servicename, serviceversion, serviceport, serviceip } =
+      getRequestArguments(req);
+
+    const key = registry.unregister(
+      servicename,
+      serviceversion,
+      serviceip,
+      serviceport
+    );
+
+    return res.json({ result: key });
   }
 );
 
